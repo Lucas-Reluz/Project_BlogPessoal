29b2c5d7f24c530012b1fb051b9d50686dc11571
Lucas-Reluz <lucas.pokemon.61@gmail.com>
2022-05-06 17:20:57 -0300

    @Lucas-Reluz [Task: Adicionado Controlador de Usuario mas ainda em atualizacao] {06/05/2022 | 17:20}

---
+++ BlogPessoal/BlogPessoal/src/controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using BlogPessoal.src.repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogPessoal.src.controllers
+{
+    [ApiController]
+    [Route("api/Users")]
+    [Produces("application/json")]
+    public class UserController : ControllerBase
+    {
+        #region Atributos
+
+        private readonly IUser _repository;
+
+        #endregion
+
+        #region Construtores
+
+        public UserController(IUser repository)
+        {
+            _repository = repository;
+        }
+
+        #endregion
+
+        [HttpGet("id/{idUsers}")]
+
+        public IActionResult GetUserbyId([FromRoute] int idUser)
+        {
+            var user = _repository.GetUserById(idUser);
+
+            if (user == null) return NotFound();
+
+            return Ok(user);
+        }
+
+        [HttpGet]
+
+        public IActionResult GetUserByName([FromQuery] string userName)
+        {
+            var users = _repository.GetUserByUsername(userName);
+            
+            if (users.Count < 1) return NoContent();
+
+            return Ok(users);
+        }
+
+        [HttpGet("email/{emailuser}")]
+        public IActionResult GetUserByEmail([FromRoute] string userEmail)
+        {
+            var user = _repository.GetUserByEmail(userEmail);
+
+            if (user == null) return NotFound();
+
+            return Ok(user);
+        }
+    }
+}
