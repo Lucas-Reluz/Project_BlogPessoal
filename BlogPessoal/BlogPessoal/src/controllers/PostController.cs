7cfcc6ea0875c8277f18bee3eac8ef3ff6d1cf63
Lucas-Reluz <lucas.pokemon.61@gmail.com>
2022-05-08 21:54:37 -0300

    @Lucas-Reluz [Task: Adicionando Controlador de Theme e Post] {08/05/2022 | 21:54}

---
+++ BlogPessoal/BlogPessoal/src/controllers/PostController.cs
@@ -0,0 +1,84 @@
+﻿using BlogPessoal.src.dtos;
+using BlogPessoal.src.repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogPessoal.src.controllers
+{
+    [ApiController]
+    [Route("api/Posts")]
+    [Produces("application/json")]
+    public class PostController : ControllerBase
+    {
+        #region Atributos
+
+        private readonly IPost _repository;
+
+        #endregion
+
+        #region Construtores
+
+        public PostController(IPost repository)
+        {
+            _repository = repository;
+        }
+
+        #endregion
+        
+        #region Metodos
+        [HttpGet("id/{idPost}")]
+        public IActionResult GetPostsById([FromRoute] int postId)
+        {
+            var post = _repository.GetPostById(postId);
+
+            if (post == null) return NotFound();
+
+            return Ok(post);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllPosts()
+        {
+            var list = _repository.GetAllPosts();
+
+            if (list.Count < 1) return NoContent();
+
+            return Ok(list);
+        }
+
+        [HttpGet("search")]
+        public IActionResult GetPostsBySearch([FromQuery] string title, [FromQuery] string descriptionTheme, [FromQuery] string creatorName)
+        {
+            var posts = _repository.GetPostsbySearch(title, descriptionTheme, creatorName);
+            
+            if(posts.Count < 1) return NoContent();
+
+            return Ok(posts);
+        }
+
+        [HttpPost]
+        public IActionResult NewPost([FromBody] NewPostDTO post)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            _repository.NewPost(post);
+            return Created($"api/Posts", post);
+        }
+
+        [HttpPut]
+        public IActionResult UpdatePost([FromBody] UpdatePostDTO upPost)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            
+            _repository.UpdatePost(upPost);
+            return Ok(upPost);
+        }
+
+        [HttpDelete("delete/{idPost}")]
+        public IActionResult DeletePost([FromRoute] int idPost)
+        {
+            _repository.DeletePost(idPost);
+            return NoContent();        
+        }
+    }
+    #endregion
+}
---
+++ BlogPessoal/BlogPessoal/src/controllers/ThemeController.cs
@@ -0,0 +1,93 @@
+﻿using BlogPessoal.src.repositories;
+using Microsoft.AspNetCore.Mvc;
+using BlogPessoal.src.dtos;
+
+namespace BlogPessoal.src.controllers
+{
+    [ApiController]
+    [Route("api/Themes")]
+    [Produces("application/json")]
+    public class ThemeController : ControllerBase
+    {
+        #region Atributos
+
+        private readonly ITheme _repository;
+
+        #endregion
+
+
+        #region Construtores
+
+        public ThemeController(ITheme repository)
+        {
+            _repository = repository;
+        }
+
+        #endregion
+
+
+        #region Metodos
+
+        [HttpGet]
+        public IActionResult GetAllThemes()
+        {
+            var list = _repository.GetAllThemes();
+
+            if (list.Count < 1) return NoContent();
+
+            return Ok(list);
+        }
+
+        [HttpGet("id/{idTheme}")]
+        public IActionResult GetThemeById([FromRoute] int idTheme)
+        {
+            var theme = _repository.GetThemeById(idTheme);
+
+            if (theme == null) return NotFound();
+
+            return Ok(theme);
+        }
+
+        [HttpGet("search")]
+        public IActionResult GetThemeByDescription([FromQuery] string descriptiontheme)
+        {
+            var themes = _repository.GetThemeByDescription(descriptiontheme);
+
+            if (themes.Count < 1) return NoContent();
+
+            return Ok(themes);
+
+        }
+
+        [HttpPost]
+        public IActionResult NewTheme([FromBody] NewThemeDTO theme)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            _repository.NewTheme(theme);
+            
+            return Created($"api/Themes", theme);
+
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTheme([FromBody] UpdateThemeDTO uptheme)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            _repository.UpdateTheme(uptheme);
+
+            return Ok(uptheme);
+        }
+
+        [HttpDelete("delete/{idTheme}")]
+        public IActionResult DeleteTheme([FromRoute] int delTheme)
+        {
+            _repository.DeleteTheme(delTheme);
+            return NoContent();
+        }
+
+        #endregion
+
+    }
+}
